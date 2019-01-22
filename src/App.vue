4fdd3753c4d0952ade6348f2398c91b26f219604
李英 <liyingt@yonyou.com>
2019-01-22 13:22:25 +0800

    add vue-router

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    liying - test  
+  <p v-if="seen">现在你看到我了</p>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  seen: true,
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,90 @@
+/* eslint-disable no-console */
+import Vue from "vue/dist/vue";
+import VueRouter from "vue-router";
+import App from './App.vue';
+Vue.use(VueRouter);
+const Home = { template: "<div>我是 Home 页</div>" };
+const Foo = { template: "<div>我是 foo 页</div>" };
+const Bar = { template: "<div>我是 bar 页</div>" };
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/foo", component: Foo },
+  { path: "/bar", component: Bar },
+  { path: "/app", component: App }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+new Vue({
+  router,
+  template: `
+  <div id="app">
+    <h1>Hello Vue-router </h1>
+    <ul>
+      <li><router-link to="/">/</router-link></li>
+      <li><router-link to="/foo">/foo</router-link></li>
+      <li><router-link to="/bar">/bar</router-link></li>
+      <li><router-link to="/app">/app</router-link></li>
+    </ul>
+    router-view content:
+    <router-view class="view"></router-view>
+  </div>
+`,
+  beforeCreate: function() {
+    console.group("beforeCreate 创建前状态===============》");
+    console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
+    console.log("%c%s", "color:red", "data   : " + this.$data); //undefined
+    console.log("%c%s", "color:red", "message: " + this.message);
+  },
+  created: function() {
+    console.group("created 创建完毕状态===============》");
+    console.log("%c%s", "color:red", "el     : " + this.$el); //undefined
+    console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
+    console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+  },
+  beforeMount: function() {
+    console.group("beforeMount 挂载前状态===============》");
+    console.log("%c%s", "color:red", "el     : " + this.$el); //已被初始化
+    console.log(this.$el);
+    console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
+    console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+  },
+  mounted: function() {
+    console.group("mounted 挂载结束状态===============》");
+    console.log("%c%s", "color:red", "el     : " + this.$el); //已被初始化
+    console.log(this.$el);
+    console.log("%c%s", "color:red", "data   : " + this.$data); //已被初始化
+    console.log("%c%s", "color:red", "message: " + this.message); //已被初始化
+  },
+  beforeUpdate: function() {
+    console.group("beforeUpdate 更新前状态===============》");
+    console.log("%c%s", "color:red", "el     : " + this.$el);
+    console.log(this.$el);
+    console.log("%c%s", "color:red", "data   : " + this.$data);
+    console.log("%c%s", "color:red", "message: " + this.message);
+  },
+  updated: function() {
+    console.group("updated 更新完成状态===============》");
+    console.log("%c%s", "color:red", "el     : " + this.$el);
+    console.log(this.$el);
+    console.log("%c%s", "color:red", "data   : " + this.$data);
+    console.log("%c%s", "color:red", "message: " + this.message);
+  },
+  beforeDestroy: function() {
+    console.group("beforeDestroy 销毁前状态===============》");
+    console.log("%c%s", "color:red", "el     : " + this.$el);
+    console.log(this.$el);
+    console.log("%c%s", "color:red", "data   : " + this.$data);
+    console.log("%c%s", "color:red", "message: " + this.message);
+  },
+  destroyed: function() {
+    console.group("destroyed 销毁完成状态===============》");
+    console.log("%c%s", "color:red", "el     : " + this.$el);
+    console.log(this.$el);
+    console.log("%c%s", "color:red", "data   : " + this.$data);
+    console.log("%c%s", "color:red", "message: " + this.message);
+  }
+}).$mount("#app");
